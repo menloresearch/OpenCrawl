72177af0ffb914c3df289e637a3bd4e561bd6b2a
Rach <54503978+justrach@users.noreply.github.com>
2025-04-01 17:19:14 +0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opencrawl",
-    version="0.1.0",
+    version="0.1.1",
     description="Integrated website crawler and content analysis library",
     author="OpenCrawl Team",
     author_email="info@opencrawl.org",
