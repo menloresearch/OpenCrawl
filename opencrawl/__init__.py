3ca72628061170ca6a7fddf93d1258875253ceb5
Rach <54503978+justrach@users.noreply.github.com>
2025-04-03 12:31:26 +0800

    Bump version to 0.1.6

--- opencrawl/__init__.py
+++ opencrawl/__init__.py
@@ -2,7 +2,7 @@
 OpenCrawl - An integrated web crawling and content analysis library
 """
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from .opencrawl import OpenCrawl, ContentAnalyzer, WebpageContent
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opencrawl",
-    version="0.1.5",
+    version="0.1.6",
     description="Integrated website crawler and content analysis library",
     author="OpenCrawl Team",
     author_email="info@opencrawl.org",
